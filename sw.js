95fe82aa910d67fd14e0a1c8200435bc557cb1ba
Gilberto Moreira <escolateologicavance@gmail.com>
2026-04-23 15:31:04 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'hazaq-estatuto-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './1.html',
+  './2.html',
+  './3.html',
+  './4.html',
+  './5.html',
+  './6.html',
+  './7.html',
+  './8.html',
+  './manifest.json',
+  './logo-192.png',
+  './logo-512.png'
+];
+
+// Instalação e Cache dos arquivos
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Ativação e limpeza de caches antigos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Estratégia de busca (Fetch): Tenta a rede, se falhar, usa o cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
